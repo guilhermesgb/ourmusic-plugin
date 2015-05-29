0146c1e31557dbd89fc21683433e3846bab4e9fd
Guilherme Baptista <guisgb13@gmail.com>
2015-05-29 03:36:32 +0200

    Resource is of 'layout' kind

--- src/android/LoginActivity.java
+++ src/android/LoginActivity.java
@@ -24,7 +24,7 @@ public class LoginActivity extends Activity {
         requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
         setContentView(getResources()
             .getIdentifier("com_ourmusic_plugin_activity_login",
-                "id", "com.ourmusic.plugin")
+                "layout", "com.ourmusic.plugin")
         );
 
         AuthenticationRequest.Builder builder = new AuthenticationRequest.Builder(OurMusicPlugin.CLIENT_ID,
