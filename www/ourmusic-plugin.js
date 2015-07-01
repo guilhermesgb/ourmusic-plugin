192b96505d7b3b6247a41abe2feb5a418a4aaba9
Guilherme Baptista <guisgb13@gmail.com>
2015-06-30 22:37:07 -0300

    callback data var name obfuscating callback itself

--- www/ourmusic-plugin.js
+++ www/ourmusic-plugin.js
@@ -2,22 +2,22 @@ module.exports = {
     login: function(success, error) {
 	cordova.exec(function(message) {
             success(message);
-        }, function(error) {
-            error(error);
+        }, function(message) {
+            error(message);
         }, "OurMusicPlugin", "login", []);
     },
     play: function(trackUri, position, token, success, error) {
 	cordova.exec(function(playerState) {
             success(playerState);
-        }, function(error) {
-            error(error);
+        }, function(message) {
+            error(message);
         }, "OurMusicPlugin", "play", [trackUri, position, token]);
     },
     pause: function(token, success, error) {
 	cordova.exec(function(playerState) {
             success(playerState);
-        }, function(error) {
-            error(error);
+        }, function(message) {
+            error(message);
         }, "OurMusicPlugin", "pause", [token]);
     }
 };
